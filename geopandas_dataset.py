e1165c91f819019ab7e5abe341c76b7519e277eb
Juliany Raiol <juliany.raiol@itriad.org.br>
2020-11-26 16:56:22 -0400

    feat: add file with lat and lng

--- geopandas_dataset.py
+++ geopandas_dataset.py
@@ -6,26 +6,33 @@ from utils import get_address_df
 from config import KEY, BASE_URL
 
 address_df = get_address_df()
+cont = 0
 
 print("INICIANDO COLETA DE ENDEREÇO")
-for address in address_df:
+
+for address in address_df["address"]:
+    cont = cont + 1
+
+    print("QUANTIDADE DE ENDEREÇOS COLETADOS: ", cont)
     try:
         params = {'sensor': 'false', 'address': address, "key": KEY}
         r = requests.get(BASE_URL, params=params)
         results = r.json()
 
+        city = results["results"][0]["address_components"][3]["long_name"]
+        state = results["results"][0]["address_components"][4]["long_name"]
         location = results["results"][0]["geometry"]["location"]
 
         with open('data/address.txt', 'a', encoding="utf-8") as f:
-                f.write(str(location["lat"]) + ',' + str(location["lng"]) + '\n')
-                f.close()
+            f.write(city + ',' + state + ',' +  str(location["lat"]) + ',' + str(location["lng"]) + '\n')
+            f.close()
 
         print(location)
     except:
-        
+
         print("FALHA AO GERAR COORDENADAS")
-        with open('data/fail.txt', 'a', encoding="utf-8") as f:
-                f.write(address + '\n')
-                f.close()
+        with open('data/address.txt', 'a', encoding="utf-8") as f:
+            f.write(",,," + '\n')
+            f.close()
 
         continue
--- utils/processing_data.py
+++ utils/processing_data.py
@@ -14,19 +14,17 @@ def load_data():
   json_format_file = [json.loads(line) for line in dat_file]
 
   df = pd.DataFrame(json_format_file)
-  
+
   return df
 
 
 def get_address_df():
-  
   df = load_data()
   item_reviewed = json_normalize(df["itemReviewed"])
-  
-  address_df = item_reviewed[["name", 
-                            "address.streetAddress", 
-                            "address.addressLocality", 
-                            "address.addressRegion", 
-                            "address.postalCode"]].apply(lambda value: " ".join(value), axis=1)
 
-  return address_df
+  columns = ["name", "address.streetAddress", "address.addressLocality", "address.addressRegion", "address.postalCode"]
+
+  address_group = item_reviewed.groupby(columns).size().reset_index()
+  address_group["address"] = address_group[columns].apply(lambda value: " ".join(value), axis=1)
+
+  return address_group
